e0a20574ec48830c1ff89135f7de9ee97ef0c888
k <cjtmarglike@naver.com>
2022-04-01 11:49:17 +0900

    react-redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import AddNumberRoot from './components/AddNumberRoot';
+import DisplayNumberRoot from './components/DisplayNumberRoot';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { number: 0 }
+  render() {
+    return (
+      <div className="App">
+        <h1>Root</h1>
+        {/* <AddNumberRoot onClick={(size)=> {
+          this.setState({ number: this.state.number + size});
+        }}></AddNumberRoot> */}
+        <AddNumberRoot onClick={function(size) {
+          this.setState({ number: this.state.number + size});
+          // console.log(this.state); //setState는 비동기로 동작하기에 호출 후 바로 확인했을 때 변경된 값이 나온다고 보장할 수 없음
+          // bind()로 바인딩된 this가 있는 새로운 함수 생성, 다른 컴포넌트로 pass할 메서드를 binding - 메서드의 재사용과 공유, 중복을 방지
+        }.bind(this)}></AddNumberRoot> 
+        <DisplayNumberRoot number={ this.state.number }></DisplayNumberRoot>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux'
+import store from './store'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* Provider를 통해 하위 컴포넌트들에 만들어둔 redux store를 공급, 개별 컴포넌트에서 import 필요 없음 */}
+    <Provider store={store}> 
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
